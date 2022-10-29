4cd74919fecb28a61937c4755c38b7077aa25196
Александр Стрюков <sanyajump16@gmail.com>
2022-10-30 00:24:49 +0300

    Initial commit

---
+++ src/test/java/org/netology/appium/UITest.java
@@ -0,0 +1,76 @@
+package org.netology.appium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import io.appium.java_client.android.AndroidDriver;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class UITest {
+    private AndroidDriver driver;
+    private AppScreen screen;
+
+    @BeforeAll
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "SomeOne");
+        desiredCapabilities.setCapability("appium:app", "C:\\Users\\1\\Desktop\\QA\\Netology\\Presentation\\MobileTesting\\Manual\\Tasks\\24Appium\\2.4 Appium\\calculator-appium-tests\\apk\\app-debug.apk");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        screen = new AppScreen(driver);
+    }
+
+    @Test
+    @Order(1)
+    public void testInputNothing() {
+        String textBefore = screen.textToBeChanged.getText();
+        screen.buttonChange.click();
+        String textAfter = screen.textToBeChanged.getText();
+        assertEquals(textBefore, textAfter);
+    }
+
+    @Test
+    @Order(2)
+    public void testInputSpace() {
+        String textBefore = screen.textToBeChanged.getText();
+        screen.inputField.sendKeys(" ");
+        screen.buttonChange.click();
+        String textAfter = screen.textToBeChanged.getText();
+        assertEquals(textBefore, textAfter);
+    }
+
+    @Test
+    @Order(3)
+    public void testOpenNewActivity() {
+        String textToSet = "Netology";
+        screen.inputField.sendKeys(textToSet);
+        screen.buttonActivity.click();
+        String textAfter = screen.activityText.getText();
+        assertEquals(textToSet, textAfter);
+    }
+
+    @AfterAll
+    public void tearDown() {
+        driver.quit();
+    }
+}
